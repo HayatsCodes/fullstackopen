003c10d25697f6e877177b6a8dccf744b70ccea4
Hayats <hayatudeenabdulrahman@gmail.com>
2023-09-20 09:06:55 +0100

    added Content Component

--- part-1/course-info/src/App.jsx
+++ part-1/course-info/src/App.jsx
@@ -7,6 +7,16 @@ const Header = (props) => {
   );
 };
 
+const Content = (props) => {
+  return (
+    <>
+      <p> {props.part1} {props.exercises1} </p>
+      <p> {props.part2} {props.exercises2} </p>
+      <p> {props.part3} {props.exercises3} </p>
+    </>
+  )
+}
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -20,7 +30,7 @@ const App = () => {
     <div>
       {/* <h1> {course} </h1> */}
       <Header course={course}></Header>
-      <p>
+      {/* <p>
         {part1} {exercises1}
       </p>
       <p>
@@ -28,7 +38,11 @@ const App = () => {
       </p>
       <p>
         {part3} {exercises3}
-      </p>
+      </p> */}
+      <Content 
+      part1={part1} part2={part2} part3={part3}
+      exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
+      ></Content>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
