d135d37424a505cf9d81f9748ceb406091ca730a
Hayats <hayatudeenabdulrahman@gmail.com>
2023-09-20 17:53:40 +0100

    Changed the course and the parts variable definitions into a single JS Object and fixed everything that breaks

--- part-1/course-info/src/App.jsx
+++ part-1/course-info/src/App.jsx
@@ -9,11 +9,6 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    // <>
-    //   <Part part={props.part1} exercises={props.exercises1}></Part>
-    //   <Part part={props.part2} exercises={props.exercises2}></Part>
-    //   <Part part={props.part3} exercises={props.exercises3}></Part>
-    // </>
     <>
     <p> {props.parts[0].name} {props.parts[0].exercises} </p>
     <p> {props.parts[1].name} {props.parts[1].exercises} </p>
@@ -21,16 +16,6 @@ const Content = (props) => {
   </>
   );
 };
-
-// const Part  = (props) => {
-//   return (
-//     <>
-//       <p> {props.part[0].name} {props.part[0].exercises} </p>
-//       <p> {props.part[1].name} {props.part[1].exercises} </p>
-//       <p> {props.part[2].name} {props.part[2].exercises} </p>
-//     </>
-//   )
-// }
 
 const Total = (props) => {
   return (
@@ -41,41 +26,46 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = "Half Stack application development";
+  // const course = "Half Stack application development";
 
-  // const part1 = {
-  //   name: 'Fundamentals of React',
-  //   exercises: 10
-  // }
-  // const part2 = {
-  //   name: 'Using props to pass data',
-  //   exercises: 7
-  // }
-  // const part3 = {
-  //   name: 'State of a component',
-  //   exercises: 14
-  // }
+  // const parts = [
+  //   {
+  //     name: 'Fundamentals of React',
+  //     exercises: 10
+  //   },
+  //   {
+  //     name: 'Using props to pass data',
+  //     exercises: 7
+  //   },
+  //   {
+  //     name: 'State of a component',
+  //     exercises: 14
+  //   }
+  // ]
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
