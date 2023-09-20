324455b212eb465082e53d66e3f8a34b9faef6f4
Hayats <hayatudeenabdulrahman@gmail.com>
2023-09-20 09:12:57 +0100

    added Total component

--- part-1/course-info/src/App.jsx
+++ part-1/course-info/src/App.jsx
@@ -14,6 +14,14 @@ const Content = (props) => {
       <p> {props.part2} {props.exercises2} </p>
       <p> {props.part3} {props.exercises3} </p>
     </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>
   )
 }
 
@@ -43,7 +51,10 @@ const App = () => {
       part1={part1} part2={part2} part3={part3}
       exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
       ></Content>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      {/* <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}>
+
+      </Total>
     </div>
   );
 };
