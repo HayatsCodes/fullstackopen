4afe5ebffe6ea886f06a662aa6e3b9bdc82d6a00
Hayats <hayatudeenabdulrahman@gmail.com>
2023-09-27 08:50:14 +0100

    allowed users to add phone numbers to phonebook

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-1234567'}
   ])
   const [newName, setNewName] = useState('')
+  const [number, setNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -19,16 +20,22 @@ const App = () => {
     }
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: number
     }
     
    
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value)
   }
 
   return (
@@ -39,12 +46,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={number} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )
