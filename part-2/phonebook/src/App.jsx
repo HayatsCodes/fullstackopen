3e6a8a27a442db30c16150eb0142e63e9b016ded
Hayats <hayatudeenabdulrahman@gmail.com>
2023-09-27 10:58:09 +0100

    Implemented a search field that can be used to filter the list of people by name

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,52 +1,77 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
-  ])
-  const [newName, setNewName] = useState('')
-  const [number, setNumber] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
+  const [newName, setNewName] = useState("");
+  const [number, setNumber] = useState("");
+  const [filter, setFilter] = useState('')
+  const [showFilter, setShowFilter] = useState(false)
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const newPerson = persons.find(person =>  person.name === newName.trim())
+    const newPerson = persons.find((person) => person.name === newName.trim());
 
     if (newPerson) {
-      alert(`${newName} is already added to phonebook`)
-      return
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
 
     const personObject = {
       name: newName,
-      number: number
-    }
-    
-   
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNumber('')
-  }
+      number: number,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNumber("");
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNumber(event.target.value)
+    setNumber(event.target.value);
+  };
+
+
+  const regex = new RegExp(`${filter}`, 'i')
+  const filteredPersons = persons.filter(person => regex.test(person.name))
+
+  const personToShow = showFilter ? filteredPersons : persons
+
+  const handleFilter = (event) => {
+    const value = event.target.value
+    setFilter(value)
+    if(!value) {
+      setShowFilter(false)
+    } else {
+      setShowFilter(true)
+    }
+    
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter: <input value={filter} onChange={handleFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input value={number} onChange={handleNumberChange}/>
+          number: <input value={number} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -54,12 +79,15 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+        {personToShow.map((person) => (
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        ))}
+        <div>debug: {filter}</div>
       </div>
     </div>
-  )
+  );
+};
 
-
-}
-
-export default App
+export default App;
