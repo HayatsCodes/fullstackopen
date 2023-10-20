1047b490cbb8de9ab939801be0594db329e7b1e1
Hayats <hayatudeenabdulrahman@gmail.com>
2023-10-20 18:59:45 +0100

    If a user is not logged in, only the login form is visible.

--- part-5/blog-list/src/App.jsx
+++ part-5/blog-list/src/App.jsx
@@ -3,11 +3,38 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername ] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <h2>Login to Application</h2>
+        <div>
+          Username: 
+          <input 
+          type="text"
+          value={username}
+          onChange={({target}) => setUsername(target.value)}
+        />
+        </div>
+        <div>
+          Password: 
+          <input
+          type="password"
+          value={password}
+          onChange={({target}) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    )
+}
 
 
   useEffect(() => {
@@ -33,30 +60,17 @@ const App = () => {
 
   return (
     <div>
-      <form onSubmit={handleLogin}>
-        <h2>Login to Application</h2>
-        <div>
-          Username: 
-          <input 
-          type="text"
-          value={username}
-          onChange={({target}) => setUsername(target.value)}
-        />
-        </div>
-        <div>
-          Password: 
-          <input
-          type="password"
-          value={password}
-          onChange={({target}) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+     {user === null 
+       ? loginForm()
+     : (
+      <>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </>
+    )}
+      
     </div>
   )
 }
