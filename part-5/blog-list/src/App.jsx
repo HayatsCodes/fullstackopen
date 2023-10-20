da8a780748feb655df6328fa7eef0feb8261703e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-10-20 19:02:49 +0100

    Logged-in user "name" being displayed

--- part-5/blog-list/src/App.jsx
+++ part-5/blog-list/src/App.jsx
@@ -64,7 +64,8 @@ const App = () => {
        ? loginForm()
      : (
       <>
-        <h2>blogs</h2>
+        <h2>Blogs</h2>
+        <p>{user.name} logged in</p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
