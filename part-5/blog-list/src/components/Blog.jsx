bf4eed7ea80906536655f4776169cf60b8b76549
Hayats <hayatudeenabdulrahman@gmail.com>
2023-10-25 16:44:13 +0100

    added a button to each blog which controls its visibility

--- part-5/blog-list/src/components/Blog.jsx
+++ part-5/blog-list/src/components/Blog.jsx
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - <em>{blog.author}</em>
-  </div>  
-)
+import { useState } from 'react'
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+  const buttonStyle = {
+    display: 'inline-block', 
+    marginLeft: '5px',
+    marginBottom: '1px'
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    maxWidth: '800px'
+  }
+
+  return (
+    <div style={blogStyle}>
+     { !view 
+      ?
+      <>
+      {blog.title} - <em>{blog.author}</em>
+      <button style={buttonStyle} onClick={() => setView(true)}>View</button>
+      </>
+      :
+      <>
+      {blog.title} - <em>{blog.author}</em>
+      <button style={buttonStyle} onClick={() => setView(false)}>Hide</button>
+      <br />
+      {blog.url}
+      <br />
+      Likes {blog.likes}
+      <button style={buttonStyle}>Like</button>
+      <br />
+      {blog.author}
+      <br />
+      </>
+    }
+    </div>
+  );
+};
+
+export default Blog;
