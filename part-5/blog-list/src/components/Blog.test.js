91940f68f6866c1c9334d4861d4194754adfb170
Hayats <hayatudeenabdulrahman@gmail.com>
2023-10-27 22:22:42 +0100

    test Other info renders when view button is clicked

--- part-5/blog-list/src/components/Blog.test.js
+++ part-5/blog-list/src/components/Blog.test.js
@@ -1,23 +1,42 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 
 import Blog from './Blog'
 
-let blog
+let blog, container
 
 beforeEach( () => {
   blog = {
     title: 'first blog',
     author: 'new author',
-    url: 'https://new-author.com'
+    url: 'https://new-author.com',
+    likes: 0
   }
+
+  container = render( <Blog blog={blog}/> ).container
 })
 
+
+
 test('Initially renders only title and author', () => {
-  const { container } = render ( <Blog blog={blog}/> )
   const div = container.querySelector('.when-view-is-false')
   expect(div).toHaveTextContent(blog.title)
   expect(div).toHaveTextContent(blog.author)
   expect(div).not.toHaveTextContent(blog.url)
+  expect(div).not.toHaveTextContent(blog.likes)
+})
+
+test('Other info renders when view button is clicked', async () => {
+  const user = userEvent.setup()
+  const button = screen.getByText('View')
+  await user.click(button)
+
+  const div = container.querySelector('.when-view-is-true')
+  expect(div).toHaveTextContent(blog.title)
+  expect(div).toHaveTextContent(blog.author)
+  expect(div).toHaveTextContent(blog.url)
+  expect(div).toHaveTextContent(blog.likes)
 })
