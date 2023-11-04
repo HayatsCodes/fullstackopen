2e099d92cf9180596bc452e407a5d10fbafab803
Hayats <hayatudeenabdulrahman@gmail.com>
2023-11-04 19:18:07 +0100

    Implemented filtering for the anecdotes that are displayed to the user using redux store

--- part-6/anecdotes/src/components/AnecdoteList.jsx
+++ part-6/anecdotes/src/components/AnecdoteList.jsx
@@ -1,17 +1,24 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux"
 import { voteOf } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state.anecdotes)
+  const filter = useSelector((state) => state.filter)
+  const dispatch = useDispatch()
+
+  const regex = new RegExp(`${filter}`, "i")
 
   const vote = (id) => {
-    dispatch(voteOf(id));
-  };
+    dispatch(voteOf(id))
+  }
+
+  const filteredAnecdotes = !filter 
+    ? anecdotes 
+    : anecdotes.filter(anecdote => regex.test(anecdote.content))
 
   return (
     <>
-      {anecdotes
+      {filteredAnecdotes
         .sort((a, b) => b.votes - a.votes)
         .map((anecdote) => (
           <div key={anecdote.id}>
@@ -23,7 +30,7 @@ const AnecdoteList = () => {
           </div>
         ))}
     </>
-  );
-};
+  )
+}
 
 export default AnecdoteList
