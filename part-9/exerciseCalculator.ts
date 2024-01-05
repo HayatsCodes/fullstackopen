3170dce56fe37d6819cb485d20eb60e93cd22a4a
Hayats <hayatudeenabdulrahman@gmail.com>
2024-01-05 10:33:51 +0100

    Eslint

--- part-9/exerciseCalculator.ts
+++ part-9/exerciseCalculator.ts
@@ -1,4 +1,4 @@
-type stringRating = 'bad, but keep trying' | 'not too bad but could be better' | 'excellent'
+type stringRating = 'bad, but keep trying' | 'not too bad but could be better' | 'excellent';
 interface ExercisesResult {
     periodLength: number,
     trainingDays: number,
@@ -10,14 +10,14 @@ interface ExercisesResult {
 }
 
 const calculateExercises = (dailyExercises: number[], target: number): ExercisesResult => {
-    const periodLength = dailyExercises.length
-    let countTrainingDays = 0
+    const periodLength = dailyExercises.length;
+    let countTrainingDays = 0;
     dailyExercises.forEach(exercise => {
         if (exercise > 0) {
-            countTrainingDays += 1
+            countTrainingDays += 1;
         }
-    })
-    const trainingDays = countTrainingDays
+    });
+    const trainingDays = countTrainingDays;
     const average = dailyExercises.reduce((a, b) => a + b, 0) / periodLength;
     const successRate = (average / target);
     let success;
@@ -25,17 +25,17 @@ const calculateExercises = (dailyExercises: number[], target: number): Exercises
     let ratingDescription: stringRating;
 
     if (successRate >= 0.5 && successRate < 1) {
-        success = false
-        rating = 2
-        ratingDescription = 'not too bad but could be better'
+        success = false;
+        rating = 2;
+        ratingDescription = 'not too bad but could be better';
     } else if (successRate < 0.5) {
-        success = false
-        rating = 1
-        ratingDescription = 'bad, but keep trying'
+        success = false;
+        rating = 1;
+        ratingDescription = 'bad, but keep trying';
     } else {
-        success = true
-        rating = 3
-        ratingDescription = 'excellent'
+        success = true;
+        rating = 3;
+        ratingDescription = 'excellent';
     }
 
     return {
@@ -46,12 +46,12 @@ const calculateExercises = (dailyExercises: number[], target: number): Exercises
         ratingDescription,
         target,
         average
-    }
-}
+    };
+};
 
-const allArguments = process.argv.slice(2)
-const target = Number(allArguments[0])
-const exercises = allArguments.slice(1).map(exercise => Number(exercise))
+const allArguments = process.argv.slice(2);
+const target = Number(allArguments[0]);
+const exercises = allArguments.slice(1).map(exercise => Number(exercise));
 
 
-console.log(calculateExercises(exercises, target))
+console.log(calculateExercises(exercises, target));
