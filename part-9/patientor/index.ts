9200ecc2b76fe358407b6a907da24d33c89ea0f5
Hayats <hayatudeenabdulrahman@gmail.com>
2024-01-09 23:09:06 +0100

    implemented GET endpoint /api/diagnoses

--- part-9/patientor/index.ts
+++ part-9/patientor/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 import cors from 'cors'
+import diagnosesRouter from './routes/diagnosesRouter';
+
 
 const app = express();
 app.use(cors())
 app.use(express.json());
+app.use('/api/diagnoses', diagnosesRouter)
 
 const PORT = 3001;
+
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part-9/patientor/types.ts
@@ -0,0 +1,10 @@
+export interface Patients{
+    id: string,
+    name: string, 
+}
+
+export interface Diagnosis {
+    code: string,
+    name: string,
+    latin?: string
+}
