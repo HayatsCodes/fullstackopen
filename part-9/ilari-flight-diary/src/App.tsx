bda29426281a240791fba908d221ca8200e7e5f0
Hayats <hayatudeenabdulrahman@gmail.com>
2024-01-15 16:49:30 +0100

    react ts exercise 1

--- part-9/ilari-flight-diary/src/App.tsx
+++ part-9/ilari-flight-diary/src/App.tsx
@@ -1,10 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from "axios";
+
+enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Stormy = 'stormy',
+  Windy = 'windy',
+}
+
+enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
+
+interface DiaryEntry {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment: string;
+}
+
 
 const App = () => {
-  const [entires, setEntries] = useState([])
+  const [entries, setEntries] = useState<DiaryEntry[]>([])
+  useEffect(() => {
+    axios.get<DiaryEntry[]>('http://localhost:3000/api/diaries')
+      .then(res => {
+        setEntries(res.data)
+      })
+  }, [])
   return (
-    <div>
+    <div className="app">
       <h1>Diary Entries</h1>
+      {entries.length > 0 && entries.map(entry => {
+        return <li key={entry.id}>
+          <h3>{entry.date}</h3>
+          <p>{entry.visibility}</p>
+          <p>{entry.weather}</p>
+        </li>
+      })}
     </div>
   )
 }
