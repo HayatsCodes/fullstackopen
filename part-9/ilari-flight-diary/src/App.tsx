144f6d1db97bb87510d1881f8546d18f71700d71
Hayats <hayatudeenabdulrahman@gmail.com>
2024-01-15 17:31:04 +0100

    react ts exercise 3

--- part-9/ilari-flight-diary/src/App.tsx
+++ part-9/ilari-flight-diary/src/App.tsx
@@ -25,17 +25,55 @@ interface DiaryEntry {
 }
 
 
+
 const App = () => {
   const [entries, setEntries] = useState<DiaryEntry[]>([])
+  const [dateInput, setDateInput] = useState('')
+  const [weatherInput, setWeatherInput] = useState('')
+  const [visibilityInput, setVisibilityInput] = useState('')
+  const [commentInput, setCommentInput] = useState('')
+  const baseUrl: string = ('http://localhost:3000/api/diaries')
   useEffect(() => {
-    axios.get<DiaryEntry[]>('http://localhost:3000/api/diaries')
+    axios.get<DiaryEntry[]>(baseUrl)
       .then(res => {
         setEntries(res.data)
       })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const response = await axios.post<DiaryEntry>(baseUrl, {
+      date: dateInput,
+      weather: weatherInput,
+      visibility: visibilityInput,
+      comment: commentInput
+    });
+    setEntries(entries.concat(response.data))
+    setDateInput('')
+    setWeatherInput('')
+    setCommentInput('')
+    setVisibilityInput('')
+  }
   return (
     <div className="app">
-      <h1>Diary Entries</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <h2>Add new Entry</h2>
+        <label>
+          Date: <input type="date" value={dateInput} onChange={({target}) => setDateInput(target.value)}/>
+        </label>
+        <label>
+          Visibility: <input type="text" value={visibilityInput} onChange={({target}) => setVisibilityInput(target.value)} />
+        </label>
+        <label>
+          Weather: <input type="text" value={weatherInput} onChange={({target}) => setWeatherInput(target.value)} />
+        </label>
+        <label>
+          Comment: <input type="text" value={commentInput} onChange={({target}) => setCommentInput(target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <h2>Diary Entries</h2>
       {entries.length > 0 && entries.map(entry => {
         return <li key={entry.id}>
           <h3>{entry.date}</h3>
